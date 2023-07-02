1d040e355458fde9f423a28e170ff7097c5c81e1
nbeney <nbeney@gmail.com>
2023-07-02 10:29:28 +0000

    .

--- codeclub_dws.py
+++ codeclub_dws.py
@@ -1,6 +1,6 @@
 import sys
 
 # Path to the folder (local or network) containing the Python packages.
-PATH_TO_PYTHON_PACKAGES = "./python-packages"
+PATH_TO_PYTHON_PACKAGES = "./python-packages"  # TODO: put final location here, eg "c:\\CodeClub\\python-packages" or "\\\\CodeClub\\python-packages"
 
 sys.path.append(PATH_TO_PYTHON_PACKAGES)
