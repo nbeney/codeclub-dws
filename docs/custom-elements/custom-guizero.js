3b87111d0e2f049a913ede3a222f7d7b007dc7e1
nbeney <nbeney@gmail.com>
2023-11-04 11:32:59 +0000

    Convert to custom-guizero.js.

---
+++ docs/custom-elements/custom-guizero.js
@@ -0,0 +1,26 @@
+function defineWidgetLinkElement(tag, widget) {
+    customElements.define(
+        tag,
+        class extends HTMLElement {
+            connectedCallback() {
+                this.innerHTML = `<code><a href="../../../offline/lawsie.github.io/guizero/${widget}/index.html">${widget}</a></code>`;
+            }
+        });
+}
+
+defineWidgetLinkElement('cc-gz-app', 'App');
+defineWidgetLinkElement('cc-gz-box', 'Box');
+defineWidgetLinkElement('cc-gz-buttongroup', 'ButtonGroup');
+defineWidgetLinkElement('cc-gz-checkbox', 'CheckBox');
+defineWidgetLinkElement('cc-gz-combo', 'Combo');
+defineWidgetLinkElement('cc-gz-drawing', 'Drawing');
+defineWidgetLinkElement('cc-gz-listbox', 'ListBox');
+defineWidgetLinkElement('cc-gz-menubar', 'MenuBar');
+defineWidgetLinkElement('cc-gz-picture', 'Picture');
+defineWidgetLinkElement('cc-gz-pushbutton', 'PushButton');
+defineWidgetLinkElement('cc-gz-slider', 'Slider');
+defineWidgetLinkElement('cc-gz-text', 'Text');
+defineWidgetLinkElement('cc-gz-textbox', 'TextBox');
+defineWidgetLinkElement('cc-gz-titlebox', 'TitleBox');
+defineWidgetLinkElement('cc-gz-waffle', 'Waffle');
+defineWidgetLinkElement('cc-gz-window', 'Window');
