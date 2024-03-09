dc60b74ff3ac581f8244925331c2ca1d0ac24e96
nbeney <nbeney@gmail.com>
2024-03-09 20:14:07 +0000

    More P5

--- activities/python-libraries/p5js-scripts/p5-math-cos-sin.js
+++ activities/python-libraries/p5js-scripts/p5-math-cos-sin.js
@@ -1,52 +1,73 @@
 new p5(p => {
-    let x;
-    let y;
     let sliderAmp;
+    let labelAmp;
     let sliderFreq;
+    let labelFreq;
+    let labelAngle;
 
     p.setup = function() {
         p.createCanvas(600, 300);
 
-        const div = p.createDiv();
-        sliderAmp = p.createSlider(-p.height/3, p.height/3, p.height/3, 5).parent(div);
-        sliderFreq = p.createSlider(0.1, 10, 1, 0.1).parent(div);
+        const divContainer = p.createDiv();
 
-        mx = 100;
-        my = 100;
+        const divAmp = p.createDiv("Amplitude: ").parent(divContainer);
+        sliderAmp = p.createSlider(-0.4 * p.height, 0.4 * p.height, 0.4 * p.height, 1).parent(divAmp);
+        p.createSpan("     ").parent(divAmp); // spacer
+        labelAmp = p.createSpan("???").parent(divAmp);
+
+        const divFreq = p.createDiv("Frequency: ").parent(divContainer);
+        sliderFreq = p.createSlider(0.1, 10, 1, 0.1).parent(divFreq);
+        p.createSpan("     ").parent(divAmp); // spacer
+        labelFreq = p.createSpan("???").parent(divFreq);
+
+        const divAngle = p.createDiv("Angle: ").parent(divContainer);
+        sliderAngle = p.createSlider(0, p.width, 60, 1).parent(divAngle);
+        p.createSpan("     ").parent(divAmp); // spacer
+        labelAngle = p.createSpan("???").parent(divAngle);
+
+        checkboxSchoolMode = p.createCheckbox("School mode (y-axis up, angles counter-clockwise)", false).parent(divContainer);
     }
 
-    function update() {
+    function updateSliderAngle() {
         if (p.mouseX >= 0 && p.mouseX < p.width && p.mouseY >= 0 && p.mouseY < p.height) {
-            mx = p.mouseX;
-            my = p.mouseY;
+            sliderAngle.value(p.mouseX);
         }
     }
 
     p.mousePressed = function() {
-        update();
+        updateSliderAngle();
     }
 
     p.mouseDragged = function() {
-        update();
+        updateSliderAngle();
+    }
+
+    function adjustForSchoolMode(y) {
+        let mult = checkboxSchoolMode.checked() ? -1 : +1;
+        return mult * y;
     }
 
     p.draw = function() {
-        // const freq = p.map(p.constrain(mx, 0, p.width), 0, p.width, 0.1, 10);  // frequency
         const freq = sliderFreq.value();
-        // const amp = p.map(p.constrain(my, 0, p.height), 0, p.height, -p.height/3, p.height/3);  // amplitude
         const amp = sliderAmp.value();
+        const angleDeg = sliderAngle.value();
+        const angleRad = p.radians(angleDeg);
+
+        labelAmp.html(amp.toFixed(1));
+        labelFreq.html(freq.toFixed(1));
+        labelAngle.html(angleDeg.toFixed(1));
         
         p.background(200);
-        p.text(`amp: ${amp.toFixed(1)}\nfreq: ${freq.toFixed(1)}`, 10, 10);
 
         p.push();
         p.translate(0, p.height/2);
         
         // Draw the axis.
+        p.textAlign(p.LEFT, p.CENTER);
         p.line(0, 0, p.width, 0);
         p.text("x", p.width-10, 10);
         p.line(1, p.height/2, 1, -p.height/2);
-        p.text("y", 10, p.height/2-10);
+        p.text("y", 5, adjustForSchoolMode(p.height/2-8));
         
         // Draw the cos graph.
         p.stroke("red");
@@ -55,25 +76,11 @@ new p5(p => {
         for (let x = 0; x < p.width; x += dx) {
             let y = amp * Math.cos(p.radians(freq * x));
             if (px !== undefined) {
-                p.line(px, py, x, y);
+                p.line(px, adjustForSchoolMode(py), x, adjustForSchoolMode(y));
             }
             px = x;
             py = y;
         }
-        
-        // Draw the global angle.
-        x = globalAngle;
-        yCos = amp * Math.cos(freq * x);
-        ySin = amp * Math.sin(freq * x);
-        p.stroke("grey");
-        p.line(p.degrees(x), -p.height/2, p.degrees(x), p.height/2);
-        p.push();
-        p.strokeWeight(5);
-        p.stroke("red");
-        p.point(p.degrees(x), yCos);
-        p.stroke("blue");
-        p.point(p.degrees(x), ySin);
-        p.pop();
         
         // Draw the sin graph.
         p.stroke("blue");
@@ -82,18 +89,32 @@ new p5(p => {
         for (let x = 0; x < p.width; x += dx) {
             let y = amp * Math.sin(p.radians(freq * x));
             if (px !== undefined) {
-                p.line(px, py, x, y);
+                p.line(px, adjustForSchoolMode(py), x, adjustForSchoolMode(y));
             }
             px = x;
             py = y;
         }
         
-        // Draw the legend.
-        p.textAlign(p.CENTER, p.BOTTOM);
+        // Draw the global angle.
+        yCos = amp * Math.cos(freq * angleRad);
+        ySin = amp * Math.sin(freq * angleRad);
+        p.stroke("grey");
+        p.line(angleDeg, -p.height/2, angleDeg, p.height/2);
+        p.push();
+        p.strokeWeight(7);
         p.stroke("red");
-        p.text(`y = amp * cos(freq * x)`, p.width/2, p.height/2-30);
+        p.point(angleDeg, adjustForSchoolMode(yCos));
         p.stroke("blue");
-        p.text(`y = amp * sin(freq * x)`, p.width/2, p.height/2-15);
+        p.point(angleDeg, adjustForSchoolMode(ySin));
+        p.pop();
+        
+        // Draw the legend.
+        p.textAlign(p.RIGHT, p.BOTTOM);
+        p.stroke("red");
+        p.text(`y = amp * cos(freq * x)`, p.width/2-10, p.height/2-8);
+        p.textAlign(p.LEFT, p.BOTTOM);
+        p.stroke("blue");
+        p.text(`y = amp * sin(freq * x)`, p.width/2+10, p.height/2-8);
 
         p.pop();
     }
--- activities/python-libraries/p5js-scripts/p5-math-trigo.js
+++ activities/python-libraries/p5js-scripts/p5-math-trigo.js
@@ -1,55 +1,55 @@
-let globalAngle = 0;
+let sliderAngle;
+let checkboxSchoolMode;
 
 new p5(p => {
-    let mx = 0;
-    let my = 0;
-
     p.setup = function() {
         p.createCanvas(300, 300);
-        mx = 300;
-        my = 300;
     }
 
-    function update() {
+    function updateSliderAngle() {
         if (p.mouseX >= 0 && p.mouseX < p.width && p.mouseY >= 0 && p.mouseY < p.height) {
-            mx = p.mouseX;
-            my = p.mouseY;
+            const dx = p.mouseX - p.width/2;
+            const dy = p.mouseY - p.height/2;
+            const angle = (Math.atan2(dy, dx) + p.TWO_PI) % p.TWO_PI; // avoid -ve values b/c of the slider
+            sliderAngle.value(p.degrees(angle));
         }
     }
 
     p.mousePressed = function() {
-        update();
+        updateSliderAngle();
     }
 
     p.mouseDragged = function() {
-        update();
+        updateSliderAngle();
+    }
+
+    function adjustForSchoolMode(y) {
+        let mult = checkboxSchoolMode.checked() ? -1 : +1;
+        return mult * y;
     }
 
     p.draw = function() {
-        const dx = mx - p.width/2;
-        const dy = my - p.height/2;
-        const angle = Math.atan2(dy, dx);
+        const angle = p.radians(sliderAngle.value()) % p.TWO_PI;
         const radius = p.height/2.5;
         const diameter = 2 * radius;
         const x = Math.cos(angle);
         const y = Math.sin(angle);
 
-        globalAngle = p.radians((p.degrees(angle) + 360) % 360);
-        
         p.background(200);
         p.push();
         
         p.translate(p.width/2, p.height/2);
         
         // Draw the axis.
+        p.textAlign(p.LEFT, p.CENTER);
         p.line(-p.width/2, 0, p.width/2, 0);
         p.text("x", p.width/2-10, 10);
-        p.text("-1", -radius-15, 10);
-        p.text("+1", radius+1, 10);
+        p.text("-1", -radius-15, -10);
+        p.text("+1", radius+3, -10);
         p.line(0, p.height/2, 0, -p.height/2);
-        p.text("y", 10, p.height/2-10);
-        p.text("-1", 10, -radius-10);
-        p.text("+1", 10, radius+10);
+        p.text("y", 5, adjustForSchoolMode(p.height/2-8));
+        p.text("-1", -15, adjustForSchoolMode(-radius-10));
+        p.text("+1", -18, adjustForSchoolMode(radius+10));
         
         // Draw the circle.
         p.noFill();
@@ -57,32 +57,36 @@ new p5(p => {
         
         // Draw the radius and the angle.
         p.stroke("green");
-        p.line(0, 0, x*radius, y*radius);
-        p.arc(0, 0, radius/2, radius/2, 0, angle);
-        p.textAlign(p.CENTER, p.BOTTOM);
-        p.text("a", 1.2 * radius/4 * Math.cos(angle/2), 1.2 * radius/4 * Math.sin(angle/2));
+        p.line(0, 0, x*radius, adjustForSchoolMode(y*radius));
+        if (checkboxSchoolMode.checked()) {
+            p.arc(0, 0, radius/2, radius/2, -angle, 0);
+        } else {
+            p.arc(0, 0, radius/2, radius/2, 0, angle);
+        }
+        p.textAlign(p.CENTER, p.CENTER);
+        p.text("a", 1.5 * radius/4 * Math.cos(angle/2), adjustForSchoolMode(1.5 * radius/4 * Math.sin(angle/2)));
         
         // Draw the cos.
         p.stroke("red");
-        p.line(0, radius*y, radius*x, radius*y);
+        p.line(0, adjustForSchoolMode(radius*y), radius*x, adjustForSchoolMode(radius*y));
         p.textAlign(p.CENTER, p.CENTER);
-        p.text("x", radius*x/2, radius*y-10);
+        p.text("x", radius*x/2, adjustForSchoolMode(radius*y-10));
         
         // Draw the sin.
         p.stroke("blue");
-        p.line(radius*x, 0, radius*x, radius*y);
+        p.line(radius*x, 0, radius*x, adjustForSchoolMode(radius*y));
         p.textAlign(p.LEFT, p.CENTER);
-        p.text("y", radius*x+10, radius*y/2);
+        p.text("y", radius*x+10, adjustForSchoolMode(radius*y/2));
 
         // Draw the legend.
         p.textAlign(p.LEFT, p.BOTTOM);
         p.stroke("green");
         let a = (p.degrees(angle) + 360) % 360;
-        p.text(`a = ${a.toFixed(0)}`, -p.width/2+15, p.height/2-45);
+        p.text(`a = ${a.toFixed(0)}`, -p.width/2+10, p.height/2-40);
         p.stroke("red");
-        p.text(`x = cos(a) = ${x.toFixed(2)}`, -p.width/2+15, p.height/2-30);
+        p.text(`x = cos(a) = ${x.toFixed(2)}`, -p.width/2+10, p.height/2-25);
         p.stroke("blue");
-        p.text(`y = sin(a) = ${y.toFixed(2)}`, -p.width/2+15, p.height/2-15);
+        p.text(`y = sin(a) = ${y.toFixed(2)}`, -p.width/2+10, p.height/2-10);
 
         p.pop();
     }
