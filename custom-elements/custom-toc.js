99b9d4b83f719fe800ddcb98648039b23b0fe70d
nbeney <nbeney@gmail.com>
2024-03-15 19:35:32 +0000

    More P5

---
+++ custom-elements/custom-toc.js
@@ -0,0 +1,23 @@
+customElements.define('cc-toc', class extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const toc = document.createElement('ul');
+        toc.className = 'toc';
+        const headings = document.querySelectorAll('h2[id]');
+        console.log(headings);
+        for (const heading of headings) {
+            console.log(heading);
+            const li = document.createElement('li');
+            li.className = 'toc-entry';
+            const a = document.createElement('a');
+            a.href = `#${heading.id}`;
+            a.textContent = heading.textContent;
+            li.appendChild(a);
+            toc.appendChild(li);
+        }
+        this.insertBefore(toc, this.firstChild);
+    }
+});
