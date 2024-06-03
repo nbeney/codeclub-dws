b8ae53611df88e80c55f7a3fff94bafbfd3d2819
nbeney <nbeney@gmail.com>
2024-06-03 07:09:19 +0100

    Fix navigation.

--- custom-elements/custom-navbar.js
+++ custom-elements/custom-navbar.js
@@ -6,34 +6,34 @@ function navbar(active, level, style) {
     let activeP5 = (active === 'p5') ? 'active' : '';
     let activePythonLibraries = (active === 'python-libraries') ? 'active' : '';
 
-    let pathPrefix = (level === '0') ? './' : '../'.repeat(level);
+    let origin = window.location.origin.replace(/\/$/, '');
 
     return `
         <nav class="navbar navbar-expand-lg ${style}">
             <div class="container-fluid">
-                <a class="navbar-brand" href="${pathPrefix}index.html">Code Club</a>
+                <a class="navbar-brand" href="${origin}/index.html">Code Club</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link ${activeHome}" href="${pathPrefix}index.html">Home</a>
+                            <a class="nav-link ${activeHome}" href="${origin}/index.html">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ${activeGuizero}" href="${pathPrefix}/activities/guizero-based/index.html">Guizero</a>
+                            <a class="nav-link ${activeGuizero}" href="${origin}/activities/guizero-based/index.html">Guizero</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ${activePygameZero}" href="${pathPrefix}/activities/pygame-zero-based/index.html">Pygame Zero</a>
+                            <a class="nav-link ${activePygameZero}" href="${origin}/activities/pygame-zero-based/index.html">Pygame Zero</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ${activeAdventurelib}" href="${pathPrefix}/activities/adventurelib-based/index.html">Adventurelib</a>
+                            <a class="nav-link ${activeAdventurelib}" href="${origin}/activities/adventurelib-based/index.html">Adventurelib</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ${activeP5}" href="${pathPrefix}/activities/p5-based/index.html">P5</a>
+                            <a class="nav-link ${activeP5}" href="${origin}/activities/p5-based/index.html">P5</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ${activePythonLibraries}" href="${pathPrefix}/activities/python-libraries/index.html">Python libraries</a>
+                            <a class="nav-link ${activePythonLibraries}" href="${origin}/activities/python-libraries/index.html">Python libraries</a>
                         </li>
                     </ul>
                 </div>
